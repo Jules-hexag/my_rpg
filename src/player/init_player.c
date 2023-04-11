dc7c97e047d1ee3a9e8211cc9cf9066043df65f3
Jules-hexag <96072490+Jules-hexag@users.noreply.github.com>
2023-04-11 15:15:03 +0200

    player is initialized

--- src/player/init_player.c
+++ src/player/init_player.c
@@ -6,10 +6,32 @@
 */
 
 #include "rpg.h"
+#include <SFML/Config.h>
+#include <SFML/Graphics/Rect.h>
+#include <SFML/Graphics/Sprite.h>
+#include <SFML/Graphics/Texture.h>
+#include <SFML/Graphics/Types.h>
+#include <SFML/System/Clock.h>
+#include <SFML/System/Vector2.h>
+
+static void create_player_sprite(player_t *player)
+{
+    sfSprite *p_sprite = sfSprite_create();
+    sfIntRect rect = {0, 0, 16, 32};
+    sfTexture *texture = sfTexture_createFromFile("res/player.png", &rect);
+    sfSprite_setTexture(p_sprite, texture, sfTrue);
+    sfSprite_setPosition(p_sprite, (sfVector2f) {player->pos.x, player->pos.y});
+    player->sprite = p_sprite;
+}
 
 player_t init_player(void)
 {
     player_t player = {0};
     player.pos = (sfVector2f) {48, 48};
+    player.mana = (barector) {30, 30};
+    player.health = (barector) {30, 30};
+    player.state = STRAIGHT;
+    player.player_clock = sfClock_create();
+    create_player_sprite(&player);
     return player;
 }
