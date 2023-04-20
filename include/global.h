181a79802e954b56d07ca3daa4ea1a123e00abf2
AntoineMaurel <antoine1.maurel@epitech.eu>
2023-04-20 18:53:40 +0200

    feat: adding missing function

--- include/global.h
+++ include/global.h
@@ -21,6 +21,29 @@ linked_list_t *remove_first_node(linked_list_t *list)
     return list;
 }
 
+linked_list_t *add_node(linked_list_t *list, void *data)
+{
+    linked_list_t *node = malloc(sizeof(linked_list_t));
+    linked_list_t *head = list;
+
+    node->data = data;
+    node->next = NULL;
+    while (list->next != NULL) {
+        list = list->next;
+    }
+    list->next = node;
+    return head;
+}
+
+linked_list_t *create_list(void *data)
+{
+    linked_list_t *list = malloc(sizeof(linked_list_t));
+
+    list->data = data;
+    list->next = NULL;
+    return list;
+}
+
 linked_list_t *add_end_node(linked_list_t *list, void *data)
 {
     if (list == NULL) {
